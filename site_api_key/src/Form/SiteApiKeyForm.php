9c6283762c0a833659b002ed37d876163bc9f4e9
rajisubbaiya <rajeswari.s@adappt.co.in>
2020-12-24 00:04:56 +0530

    added the form seting for site api key

--- site_api_key/src/Form/SiteApiKeyForm.php
+++ site_api_key/src/Form/SiteApiKeyForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\system\Form\SiteInformationForm;
 
 /**
- * Settings form for adding site api key.
+ * Setting for adding the site api key
  */
 class SiteApiKeyForm extends SiteInformationForm {
 
@@ -29,31 +29,31 @@ class SiteApiKeyForm extends SiteInformationForm {
   /**
    * {@inheritdoc}
    */
-  // public function buildForm(array $form, FormStateInterface $form_state) {
-  //   $form = parent::buildform($form, $form_state);
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildform($form, $form_state);
 
-  //   // Load the Site Information configuration and extract Site API Key from it.
-  //   $config = $this->config('system.site');
-  //   $apikey = $config->get('siteapikey');
+    // Getting the site information key value and setting to siteapikey
+    $config = $this->config('system.site');
+    $apikey = $config->get('siteapikey');
 
-  //   // Set a default value if Key is not set.
-  //   if (empty($apikey)) {
-  //     $apikey = 'No API Key yet';
-  //   }
-  //   // Create textfield for storing Site API Key.
-  //   $form['site_information']['siteapikey'] = [
-  //     '#type' => 'textfield',
-  //     '#title' => $this->t('Site API Key'),
-  //     '#description' => $this->t('The private key to access JSON representation of page nodes.'),
-  //     '#default_value' => $apikey,
-  //   ];
+    // Set default value to No API key yet
+    if (empty($apikey)) {
+      $apikey = 'No API Key yet';
+    }
+    // Create textfield for Site API Key.
+    $form['site_information']['siteapikey'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Site API Key'),
+      '#description' => $this->t('The site api key to access JSON representation of page nodes.'),
+      '#default_value' => $apikey,
+    ];
 
-  //   // Change the submit button title.
-  //   $form['actions']['submit']['#value'] = $this->t('Update Configuration');
+    // changing the submit to update configuration
+    $form['actions']['submit']['#value'] = $this->t('Update Configuration');
 
-  //   // Return the altered form.
-  //   return $form;
-  // }
+    // Return the altered form.
+    return $form;
+  }
 
   /**
    * {@inheritdoc}
@@ -61,17 +61,17 @@ class SiteApiKeyForm extends SiteInformationForm {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    // Get the submitted form values.
+    // Getting the submitted values.
     $values = $form_state->getValues();
 
-    // Save the submitted Site API Key in configuration.
+    // Save the entered site API key in configuration.
     $this->config('system.site')
       ->set('siteapikey', $values['siteapikey'])
       ->save();
 
     // Set the message based on the submitted Site API Key.
     if (!empty($values['siteapikey']) && $values['siteapikey'] != "No API Key yet") {
-      $this->messenger()->addStatus($this->t('The Site API Key has been saved with value "@key".', ['@key' => $values['siteapikey']]));
+      $this->messenger()->addStatus($this->t('Your site api key has been successfully created "@key".', ['@key' => $values['siteapikey']]));
     }
     else {
       $this->messenger()->addStatus($this->t('Site API Key is not added yet.'));
